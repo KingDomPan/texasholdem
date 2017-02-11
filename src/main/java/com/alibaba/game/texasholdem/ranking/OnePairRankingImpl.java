c258471ba7932b2659d55d4f8608c936aac08c52
panqingdao <panqingdao@weidian.com>
2017-02-11 17:18:33 +0800

    一对牌型校验

---
+++ src/main/java/com/alibaba/game/texasholdem/ranking/OnePairRankingImpl.java
@@ -0,0 +1,41 @@
+package com.alibaba.game.texasholdem.ranking;
+
+import com.alibaba.game.texasholdem.Player;
+import com.alibaba.game.texasholdem.RuleRankingEnum;
+
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Class {@code OnePairRankingImpl}
+ * 解析玩家手中的牌是不是一对(2+1+1+1)
+ */
+public class OnePairRankingImpl extends AbstractRanking {
+
+    protected RankingResult doResolve(Player player) {
+
+        RankingResult result = null;
+        Map<Integer, Integer> rankCount = player.getCardsRankCountMap();
+
+        boolean hasOne = false;
+
+        if (rankCount.size() == 4) {
+            Iterator<Map.Entry<Integer, Integer>> it = rankCount.entrySet().iterator();
+            while(it.hasNext()) {
+                Map.Entry<Integer, Integer> next = it.next();
+                if (next.getValue() == 2 || next.getValue() == 1) {
+                    hasOne = true;
+                    break;
+                }
+            }
+        }
+
+        if (hasOne) {
+            result = new RankingResult();
+            result.setRankingEnum(RuleRankingEnum.ONE_PAIR);
+        }
+
+        return result;
+    }
+
+}
---
+++ src/test/java/com/alibaba/game/texasholdem/ranking/OnePairRankingImplTest.java
@@ -0,0 +1,49 @@
+package com.alibaba.game.texasholdem.ranking;
+
+import com.alibaba.game.texasholdem.*;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class OnePairRankingImplTest extends TestCase {
+
+
+    @Test
+    public void testIsOnePairRanking() {
+
+        Card card13 = new Card(CardSuitEnum.HEARTS, CardRankEnum.CARD_KING);
+        Card card1313 = new Card(CardSuitEnum.DIAMONDS, CardRankEnum.CARD_KING);
+        Card card12= new Card(CardSuitEnum.SPADES, CardRankEnum.CARD_QUEUE);
+        Card card11 = new Card(CardSuitEnum.CLUBS, CardRankEnum.CARD_JACK);
+        Card card9 = new Card(CardSuitEnum.HEARTS, CardRankEnum.CARD_NINE);
+
+        Card otherSuitCard = new Card(CardSuitEnum.DIAMONDS, CardRankEnum.CARD_TEN);
+
+        Player p = new Player();
+        p.addCard(card13);
+        p.addCard(card1313);
+        p.addCard(card11);
+        p.addCard(card12);
+        p.addCard(card9);
+
+        IRanking ranking = new OnePairRankingImpl();
+
+        RankingResult result = ranking.resolve(p);
+        assertTrue(result != null);
+
+        assertEquals(result.getRankingEnum(), RuleRankingEnum.ONE_PAIR);
+
+        Player p2 = new Player();
+        p2.addCard(card13);
+        p2.addCard(card11);
+        p2.addCard(card12);
+        p2.addCard(card9);
+        p2.addCard(otherSuitCard);
+
+        IRanking ranking2 = new OnePairRankingImpl();
+
+        RankingResult result2 = ranking2.resolve(p2);
+        assertTrue(result2 == null);
+
+    }
+
+}
