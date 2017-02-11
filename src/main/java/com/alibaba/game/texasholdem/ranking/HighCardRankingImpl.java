4709dee8b005c7df92562a800f1d3b2845e7e3f3
panqingdao <panqingdao@weidian.com>
2017-02-11 17:36:00 +0800

    高牌牌型校验

---
+++ src/main/java/com/alibaba/game/texasholdem/ranking/HighCardRankingImpl.java
@@ -0,0 +1,45 @@
+package com.alibaba.game.texasholdem.ranking;
+
+import com.alibaba.game.texasholdem.Card;
+import com.alibaba.game.texasholdem.Constants;
+import com.alibaba.game.texasholdem.Player;
+import com.alibaba.game.texasholdem.RuleRankingEnum;
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Class {@code HighCardRankingImpl}
+ * 解析玩家手中的牌是不是单牌(1+1+1+1+1)
+ */
+public class HighCardRankingImpl extends AbstractRanking {
+
+    protected RankingResult doResolve(Player player) {
+
+        RankingResult result = null;
+        Map<Integer, Integer> rankCount = player.getCardsRankCountMap();
+
+        boolean allOne = false;
+
+        if (rankCount.size() == Constants.HAND_CARD_NUMERS) {
+            List<Card> cards = player.getCards();
+            if (!this.isSameSuit(cards)) {
+                Card maxCard = cards.get(0);
+                Card minCard = cards.get(cards.size() - 1);
+                if (maxCard.getRank().getNumber() - minCard.getRank().getNumber() >= Constants.HAND_CARD_NUMERS) {
+                    allOne = true;
+                }
+            }
+        }
+
+        if (allOne) {
+            result = new RankingResult();
+            result.setRankingEnum(RuleRankingEnum.HIGH_CARD);
+        }
+
+        return result;
+    }
+
+}
---
+++ src/test/java/com/alibaba/game/texasholdem/ranking/HighCardRankingImplTest.java
@@ -0,0 +1,50 @@
+package com.alibaba.game.texasholdem.ranking;
+
+import com.alibaba.game.texasholdem.*;
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class HighCardRankingImplTest extends TestCase {
+
+
+    @Test
+    public void testIsOnePairRanking() {
+
+        Card card13 = new Card(CardSuitEnum.HEARTS, CardRankEnum.CARD_KING);
+        Card card12= new Card(CardSuitEnum.SPADES, CardRankEnum.CARD_QUEUE);
+        Card card11 = new Card(CardSuitEnum.CLUBS, CardRankEnum.CARD_JACK);
+        Card card10 = new Card(CardSuitEnum.CLUBS, CardRankEnum.CARD_TEN);
+        Card card9 = new Card(CardSuitEnum.HEARTS, CardRankEnum.CARD_NINE);
+        Card card8 = new Card(CardSuitEnum.HEARTS, CardRankEnum.CARD_EIGHT);
+
+        Card otherSuitCard = new Card(CardSuitEnum.DIAMONDS, CardRankEnum.CARD_THREE);
+
+        Player p = new Player();
+        p.addCard(card13);
+        p.addCard(card12);
+        p.addCard(card11);
+        p.addCard(card10);
+        p.addCard(card8);
+
+        IRanking ranking = new HighCardRankingImpl();
+
+        RankingResult result = ranking.resolve(p);
+        assertTrue(result != null);
+
+        assertEquals(result.getRankingEnum(), RuleRankingEnum.HIGH_CARD);
+
+        Player p2 = new Player();
+        p2.addCard(card13);
+        p2.addCard(card12);
+        p2.addCard(card11);
+        p2.addCard(card10);
+        p2.addCard(card9);
+
+        IRanking ranking2 = new HighCardRankingImpl();
+
+        RankingResult result2 = ranking2.resolve(p2);
+        assertTrue(result2 == null);
+
+    }
+
+}
