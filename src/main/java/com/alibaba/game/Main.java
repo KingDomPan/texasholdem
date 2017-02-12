2c5aad4e393270f1602f85c7419d0803358243d4
panqingdao <panqingdao@weidian.com>
2017-02-12 14:20:29 +0800

    修改main函数

--- src/main/java/com/alibaba/game/Main.java
+++ src/main/java/com/alibaba/game/Main.java
@@ -1,8 +1,11 @@
-package com.alibaba.game.texasholdem;
+package com.alibaba.game;
+
+import com.alibaba.game.texasholdem.Dealer;
+import com.alibaba.game.texasholdem.Player;
 
 import java.util.List;
 
-public class GameStartor {
+public class Main {
 
     public static void main(String[] args) {
         Dealer d = new Dealer();
@@ -14,6 +17,7 @@ public class GameStartor {
         d.join(you);
 
         d.start();
+        d.showHand();
 
         List<Player> players = d.getRankingPlayers();
 
